0fedc1a759e7fd8396773a8694c17a531b0fcde8
oluoko <brianotieno586@gmail.com>
2024-11-07 20:34:35 +0300

    Chess Game: Abstract Class Piece

---
+++ src/app/chess-logic/models.ts
@@ -0,0 +1,24 @@
+export enum Color {
+  Black,
+  White,
+}
+
+export type Coords = {
+  x: number;
+  y: number;
+};
+
+export enum FENChar {
+  BlackKing = 'K',
+  BlackQueen = 'Q',
+  BlackRook = 'R',
+  BlackBishop = 'B',
+  BlackKnight = 'N',
+  BlackPawn = 'P',
+  WhiteKing = 'k',
+  WhiteQueen = 'q',
+  WhiteRook = 'r',
+  WhiteBishop = 'b',
+  WhiteKnight = 'n',
+  WhitePawn = 'p',
+}
