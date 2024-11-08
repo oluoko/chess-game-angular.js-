55b199a5b4df3cb26433676216a7a6925d9e081a
oluoko <brianotieno586@gmail.com>
2024-11-08 09:23:38 +0300

    Chess Game: Implementing method to check if the player is in check

--- src/app/chess-logic/chess-board.ts
+++ src/app/chess-logic/chess-board.ts
@@ -9,6 +9,7 @@ import { Bishop } from './pieces/bishop';
 
 export class ChessBoard {
   private chessBoard: (Piece | null)[][];
+  private readonly chessBoardSize: number = 8;
   private _playerColor = Color.White;
 
   constructor() {
@@ -74,5 +75,59 @@ export class ChessBoard {
 
   public static isSquareDark(x: number, y: number): boolean {
     return (x % 2 === 0 && y % 2 === 0) || (x % 2 === 1 && y % 2 === 1);
+  }
+
+  private areCoordsValid(x: number, y: number): boolean {
+    return (
+      x >= 0 && x < this.chessBoardSize && y >= 0 && y < this.chessBoardSize
+    );
+  }
+
+  public isInCheck(playerColor: Color): boolean {
+    for (let x = 0; x < this.chessBoardSize; x++) {
+      for (let y = 0; y < this.chessBoardSize; y++) {
+        const piece: Piece | null = this.chessBoard[x][y];
+        if (!piece || piece.color === playerColor) continue;
+
+        for (const { x: dx, y: dy } of piece.directions) {
+          let newX: number = x + dx;
+          let newY: number = y + dy;
+
+          if (!this.areCoordsValid(newX, newY)) continue;
+
+          if (
+            piece instanceof Pawn ||
+            piece instanceof Knight ||
+            piece instanceof King
+          ) {
+            // Pawns can only attack diagonally
+            if (piece instanceof Pawn && dy === 0) continue;
+
+            const attackedPiece: Piece | null = this.chessBoard[newX][newY];
+            if (
+              attackedPiece instanceof King &&
+              attackedPiece.color === playerColor
+            )
+              return false;
+          } else {
+            while (this.areCoordsValid(newX, newY)) {
+              const attackedPiece: Piece | null = this.chessBoard[newX][newY];
+              if (
+                attackedPiece instanceof King &&
+                attackedPiece.color === playerColor
+              )
+                return false;
+
+              if (attackedPiece !== null) break;
+
+              newX += dx;
+              newY += dy;
+            }
+          }
+        }
+      }
+    }
+
+    return false;
   }
 }
