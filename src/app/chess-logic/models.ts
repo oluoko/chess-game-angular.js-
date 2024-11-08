179cc6f6b9c31b775746c5465d2266dd22a3d074
oluoko <brianotieno586@gmail.com>
2024-11-08 07:27:56 +0300

    Chess Game: Working on displaying the chess board on the Browser

--- src/app/chess-logic/models.ts
+++ src/app/chess-logic/models.ts
@@ -22,3 +22,18 @@ export enum FENChar {
   WhiteKnight = 'n',
   WhitePawn = 'p',
 }
+
+export const pieceImagePaths: Readonly<Record<FENChar, string>> = {
+  [FENChar.BlackKing]: 'assets/pieces/black-king.svg',
+  [FENChar.BlackQueen]: 'assets/pieces/black-queen.svg',
+  [FENChar.BlackRook]: 'assets/pieces/black-rook.svg',
+  [FENChar.BlackBishop]: 'assets/pieces/black-bishop.svg',
+  [FENChar.BlackKnight]: 'assets/pieces/black-knight.svg',
+  [FENChar.BlackPawn]: 'assets/pieces/black-pawn.svg',
+  [FENChar.WhiteKing]: 'assets/pieces/white-king.svg',
+  [FENChar.WhiteQueen]: 'assets/pieces/white-queen.svg',
+  [FENChar.WhiteRook]: 'assets/pieces/white-rook.svg',
+  [FENChar.WhiteBishop]: 'assets/pieces/white-bishop.svg',
+  [FENChar.WhiteKnight]: 'assets/pieces/white-knight.svg',
+  [FENChar.WhitePawn]: 'assets/pieces/white-pawn.svg',
+};
